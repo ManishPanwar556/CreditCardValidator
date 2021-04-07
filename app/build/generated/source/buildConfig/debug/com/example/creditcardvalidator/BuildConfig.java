5ce4ea0a293a99cd1d8d4d92a7df53c30e4b3624
Manish Panwar <panwarmanish843@gmail.com>
2021-04-07 08:05:56 +0530

    Added build

---
+++ app/build/generated/source/buildConfig/debug/com/example/creditcardvalidator/BuildConfig.java
@@ -0,0 +1,12 @@
+/**
+ * Automatically generated file. DO NOT MODIFY
+ */
+package com.example.creditcardvalidator;
+
+public final class BuildConfig {
+  public static final boolean DEBUG = Boolean.parseBoolean("true");
+  public static final String APPLICATION_ID = "com.example.creditcardvalidator";
+  public static final String BUILD_TYPE = "debug";
+  public static final int VERSION_CODE = 1;
+  public static final String VERSION_NAME = "1.0";
+}
